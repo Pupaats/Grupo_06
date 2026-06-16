74ee9b2eb92cdc83eb6ccff2c3eade3a0907f1fb
Pupaats <josuesepulveda2007@gmail.com>
2026-06-16 19:27:31 -0400

    Expandi el arbol nuevamente, ahora con los metodos Buscar y comparar en Ascii, los documente completamente. tambien añadi los getters y setters en NodoArbol

--- src/grupo_06/Arbol.java
+++ src/grupo_06/Arbol.java
@@ -6,7 +6,9 @@ public class Arbol {
     public Arbol(){
         this.raiz = null;
     }
-    
+    public void insertar(Reclamos reclamo){
+        raiz = insertarRecursivo(raiz, reclamo);
+    }
     private NodoArbol insertarRecursivo(NodoArbol nodo, Reclamos reclamo){
         //Caso base : que no tenga nada
         if (nodo == null){
@@ -27,5 +29,55 @@ public class Arbol {
             //Si ya habia un reclamo, no permite que se repita
         }
         return nodo;
+        }
+    
+
+    public Reclamos buscar(String Nombre){
+        if(raiz == null){
+            System.out.println("No existe el reclamo");
+            //Si la raíz es nula, el árbol está vacío y no hay nada que buscar
+        }
+        NodoArbol actual = raiz; //Nodo auxiliar para empezar a recorrer el árbol desde la raiz
+        while(actual != null){ //loop para navegar el árbol mientras queden nodos por revisar
+            if(actual.getReclamo().getNombre().equals(Nombre)){
+                return actual.getReclamo();
+            //Si el nombre buscado es exactamente igual al del nodo actual retorna correctamente
+            }
+            char[] aux = actual.reclamo.getNombre().toCharArray(); //Si no es igual preparamos la conversion a char
+            // Si comparandoAscii devuelve true, significa que el nombre buscado es menor
+            if(comparandoAscii(Nombre.toCharArray(), aux)){
+                actual = actual.getIzquierda();// Baja por el subárbol izquierdo
+            } else {
+                actual = actual.getDerecha();// Baja por el subárbol derecho
+            }
+        }
+        System.out.println("No se encontro el reclamo");
+        return null;
+    }
+    
+    
+    
+    public static boolean comparandoAscii(char [] cadena, char[] comparando){
+        //Esto sirve para comparar 2 caracteres basandose en su valor Ascii
+        //Elige la que va primero en orden alfabetico
+        int valor1 = 0;
+        int valor2 = 0;
+        int posicion = 0;
+        /*Avanza palabra por palabra, si ambas son iguales sigue avanzando
+        si no, compara directamente o cuando llegue al final de la palabra
+        para evitar un desbordamiento
+        */
+        while(valor1 == valor2 && posicion < cadena.length && posicion < comparando.length){
+            valor1 = (int)cadena[posicion]; //convierte el caracter de la palabra a Ascii
+            valor2 = (int)comparando[posicion];
+            posicion++; //Avanzar a la siguiente posicion
+        }
+        //Aqui evalua, la palabra con menor valor numerico va a la izquierda
+        // y la con mayor valor numerico a la derecha
+        if(valor1 < valor2){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
--- src/grupo_06/NodoArbol.java
+++ src/grupo_06/NodoArbol.java
@@ -13,5 +13,38 @@ public class NodoArbol {
         this.reclamo = reclamo;
         this.izquierda = null;
         this.derecha = null;
+       
     }
+    //Getters
+    public Reclamos getReclamo() {
+        return reclamo;
+    }
+
+    public NodoArbol getIzquierda() {
+        return izquierda;
+    }
+
+    public NodoArbol getDerecha() {
+        return derecha;
+    }
+    
+    //Setters
+    public void setReclamo(Reclamos reclamo) {
+        this.reclamo = reclamo;
+    }
+
+    public void setIzquierda(NodoArbol izquierda) {
+        this.izquierda = izquierda;
+    }
+
+    public void setDerecha(NodoArbol derecha) {
+        this.derecha = derecha;
+    }
+    
+    @Override
+    public String toString() {
+        return reclamo.getNombre();
+    }    
+    
+    
 }
