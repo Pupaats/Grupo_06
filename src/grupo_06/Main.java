853227aa7b02e198e52718d2c3833c051241347c
Pupaats <josuesepulveda2007@gmail.com>
2026-06-16 17:17:11 -0400

    Añadi las clases de los nodos para los arboles y listas tambien añadi el metodo MostrarInfo a Reclamos

--- src/grupo_06/Main.java
+++ src/grupo_06/Main.java
@@ -1,9 +1,10 @@
 package grupo_06;
 
 public class Main {
-
-
-    public static void main(String[] args) {
-    }
     
+    public static void main(String[] args) {
+        //Aqui si alguien podria avanzar el main porfavor
+        System.out.println("Gestion de reclamos de la municipalidad San Rafael");
+    }
+
 }
