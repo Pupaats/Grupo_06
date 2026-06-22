f6551d13369dbad3b1ad3f453f063f9941f89fc1
zPanxo <zpanxobombsquad@gmail.com>
2026-06-22 00:24:12 -0400

    Cambio al método consultarReclamos para que ordene según prioridad

--- src/grupo_06/ListaReclamos.java
+++ src/grupo_06/ListaReclamos.java
@@ -3,7 +3,7 @@ package grupo_06;
 public class ListaReclamos {
     private NodoLista iniciolista;
     private NodoLista finallista;
-    private int contador;
+    private int cantidadReclamos = 0;
 
     public ListaReclamos() {
         this.iniciolista = null;
@@ -20,10 +20,10 @@ public class ListaReclamos {
             finallista.siguiente = nuevoNodo;
             finallista = nuevoNodo;
         }
-        contador ++;
+        cantidadReclamos ++;
     }
     
-    
+    /*
     public void consultarReclamos(){
         NodoLista NodoActual = iniciolista;
         
@@ -37,12 +37,48 @@ public class ListaReclamos {
             }
         }        
     }
-   
-    public int obtenerTamaño(){
-        return contador; // cambio para que sea o(1)
+   */
+    
+    public void consultarReclamos(){
+        
+        if(iniciolista == null){
+            System.out.println("Aún no hay reclamos registrados.");
+        }else{
+            NodoLista NodoActual = iniciolista;
+
+            Reclamos[] arreglo = new Reclamos[cantidadReclamos];
+
+            // Agregamos en cada indice un reclamo
+            for(int i=0; i<cantidadReclamos; i++){
+                arreglo[i] = NodoActual.reclamo;
+                NodoActual = NodoActual.siguiente;
+            }
+
+            // Ordenamos el arreglo según el nivel de prioridad utilizando Insertion
+            int n = arreglo.length;
+            for(int i=1; i<n; i++){
+                Reclamos key = arreglo[i];
+                int j = i-1;
+
+                while(j >= 0 && arreglo[j].getNivelPrioridad() > key.getNivelPrioridad()){
+                    arreglo[j+1] = arreglo[j];
+                    j--;
+                }
+                arreglo[j+1] = key;
+            }     
+
+            System.out.println("Reclamos registrados: ");
+            for(Reclamos reclamo : arreglo){
+                reclamo.mostrarInfo();
+            }
+        }
     }
     
-    // Extra
+    public int obtenerTamaño(){
+        return cantidadReclamos; // cambio para que sea o(1)
+    }
+    
+    // Extra ---> Eliminar luego
     public void mostrarCodigos(){
         NodoLista NodoActual = iniciolista;
         
