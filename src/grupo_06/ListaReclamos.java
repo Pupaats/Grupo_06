83d39410dd6b882e9411548ab8ce3c582e882e98
zPanxo <zpanxobombsquad@gmail.com>
2026-06-23 20:22:29 -0400

    Se regresó el método de eliminar, y se arreglaron pequeños errores

--- src/grupo_06/ListaReclamos.java
+++ src/grupo_06/ListaReclamos.java
@@ -1,15 +1,19 @@
 package grupo_06;
 
+
+// Falta el eliminar reclamo y la busqueda binaria, lo vemos despues
 public class ListaReclamos {
     private NodoLista iniciolista;
     private NodoLista finallista;
-    private int cantidadReclamos = 0;
+    private int cantidadReclamos;
 
-    public ListaReclamos() {
+    public ListaReclamos(){
         this.iniciolista = null;
         this.finallista = null;
+        this.cantidadReclamos = 0;
     }
     
+    //creo que tiene una complejidad de 0(1), ya que inserta al final con el puntero
     public void registrarReclamo(Reclamos reclamo){
         NodoLista nuevoNodo = new NodoLista(reclamo);
         
@@ -22,8 +26,29 @@ public class ListaReclamos {
         }
         cantidadReclamos ++;
     }
+    //Busqueda Sequencial
+    /* cuando usariamos la busqueda secuencial ? cuando la lista no esta ordenada o cuando se busca
+    una sola vez, cuando no requiere ordenamiento previo o cuando las listas son pequeñas
+    tiene de complejidad O(n) porque en el peor de los casos se tendria que recorrer la lista completa  
+    */
     
+    public Reclamos buscarReclamo(String codigoUnico){
+        NodoLista actual = iniciolista;
+        while(actual != null){
+            if(actual.reclamo.getCodigoUnico().equals(codigoUnico)){
+                return actual.reclamo;
+            }
+            actual = actual.siguiente;
+        }
+        return null; // no lo encontro
+    } 
+    
+    //Busqueda Binaria
     /*
+    TODO: hacer la bsuqueda binaria, me la hago mañana que ahora toy cansado
+    */
+    
+    /* No se porque esta esto aca xd
     public void consultarReclamos(){
         NodoLista NodoActual = iniciolista;
         
@@ -39,6 +64,37 @@ public class ListaReclamos {
     }
    */
     
+    
+   public boolean eliminarReclamo(String codigoUnico){
+        NodoLista NodoActual = iniciolista;
+        NodoLista NodoAnterior = null;
+ 
+        while(NodoActual != null){
+            if(NodoActual.reclamo.getCodigoUnico().equals(codigoUnico)){
+                if(NodoAnterior == null && NodoActual == finallista){
+                    iniciolista = null;
+                    finallista = null;
+                }else if(NodoAnterior == null){
+                    iniciolista = NodoActual.siguiente;
+                }else if(NodoActual == finallista){
+                    NodoAnterior.siguiente = null;
+                    finallista = NodoAnterior;
+                }else{
+                    NodoAnterior.siguiente = NodoActual.siguiente;
+                }
+                cantidadReclamos--;
+                return true;
+            }
+            NodoAnterior = NodoActual;
+            NodoActual = NodoActual.siguiente;
+        }
+ 
+        System.out.println("Hubo un error, hay coincidencias con el código: " + codigoUnico);
+        return false;
+    }    
+    
+    //Muestra todos los reclamos ordenados por prioridad usando el InsertionSort
+    // tiene de complejidad el O(n^2) por el ordenamiento cuadratico
     public void consultarReclamos(){
         
         if(iniciolista == null){
@@ -53,8 +109,9 @@ public class ListaReclamos {
                 arreglo[i] = NodoActual.reclamo;
                 NodoActual = NodoActual.siguiente;
             }
-
-            // Ordenamos el arreglo según el nivel de prioridad utilizando Insertion
+ 
+            // Ordenamos el arreglo según el nivel de prioridad utilizando Insertion O(n^2) en el peor caso
+            // y O(n) si ya esta ordenado
             int n = arreglo.length;
             for(int i=1; i<n; i++){
                 Reclamos key = arreglo[i];
